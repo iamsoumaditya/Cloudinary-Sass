6c7602efb020460cddb928c8fd6a6c53eef34d1c
iamsoumaditya <iamsoumadityaroy@gmail.com>
2026-02-07 02:39:52 +0530

    feat: update environment variable configuration for Cloudinary API keys

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -14,7 +14,7 @@ const prisma = new PrismaClient({
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
